d23455b39e5c9ae453298e37c3bf242a9bcb4290
davidsallusti <116444851+davidsallusti@users.noreply.github.com>
2022-12-10 09:35:40 -0500

    new

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3000/imageurl', {
+      fetch('https://playful-yeot-810762.netlify.app/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -74,7 +74,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://playful-yeot-810762.netlify.app/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
